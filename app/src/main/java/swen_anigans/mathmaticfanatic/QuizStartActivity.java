e5f26f12601a449763355541542dde3d46b1128f
Grant Gadomski <gg2727@rit.edu>
2016-11-02 15:20:02 -0400

    Created files for Quiz Start page.

---
+++ app/src/main/java/swen_anigans/mathmaticfanatic/QuizStartActivity.java
@@ -0,0 +1,14 @@
+package swen_anigans.mathmaticfanatic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuizStartActivity extends AppCompatActivity {
+
+    //TODO: Figure out where data on which numbers to quiz on will come from.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_start);
+    }
+}
