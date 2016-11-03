767279497bfc4a594f27ee6322c44b46fd21c05c
Sean Klei <stk1151@rit.edu>
2016-11-03 14:52:52 -0400

    Added the work done on my other project. It took a few hours to actually get it to get it here. Whew...

--- app/src/main/java/swen_anigans/mathematicfanatic/MainActivity.java
+++ app/src/main/java/swen_anigans/mathematicfanatic/MainActivity.java
@@ -1,7 +1,10 @@
 package swen_anigans.mathematicfanatic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBar;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar =(Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 }
