18d1aa47d94e1b2ed0b0d1596f90b33f73e3e5df
Sean Klei <stk1151@rit.edu>
2016-11-08 17:58:40 -0500

    Updated the student settings to have the custom actionbar

--- app/src/main/java/swen_anigans/mathematicfanatic/StudentSettings.java
+++ app/src/main/java/swen_anigans/mathematicfanatic/StudentSettings.java
@@ -2,12 +2,27 @@ package swen_anigans.mathematicfanatic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 public class StudentSettings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.GuardianTheme);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar.setBackgroundResource(R.color.guardianPrimary);
+        setSupportActionBar(myToolbar);
         setContentView(R.layout.activity_student_settings);
+
+        // This is where we would set the text on the toolbar
+        // to match the student's name
+        TextView toolbarText = (TextView) findViewById(R.id.toolbar_text);
+        toolbarText.setText("{STUDENT_NAME}");
+
+
+
+
     }
 }
