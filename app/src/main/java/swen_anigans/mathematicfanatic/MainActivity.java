e2876baf1f1e16b3be74f90da0a5cb8e6a15f1a5
Sean Klei <stk1151@rit.edu>
2016-11-08 11:08:15 -0500

    Have it so we add multiple dynamic buttons

--- app/src/main/java/swen_anigans/mathematicfanatic/MainActivity.java
+++ app/src/main/java/swen_anigans/mathematicfanatic/MainActivity.java
@@ -24,34 +24,34 @@ public class MainActivity extends AppCompatActivity {
 
         // TODO: Determine where we're getting the data for the
         //It will probably be something like....
-        //for(Student student: StudentManager.getAllStudents())
-        //{
-        Button studentButton = new Button(this);
-        Button progressReportButton = new Button(this);
-        // replace the hardcoded text with the name of the student
-        studentButton.setBackgroundResource(R.drawable.long_button);
-        studentButton.setTextSize(15);
-        studentButton.setText("Student X");
+        for(int i = 0; i < 6; i++)
+        {
+            Button studentButton = new Button(this);
+            Button progressReportButton = new Button(this);
+            // replace the hardcoded text with the name of the student
+            studentButton.setBackgroundResource(R.drawable.long_button);
+            studentButton.setTextSize(15);
+            studentButton.setText("Student " + i);
 
-        progressReportButton.setBackgroundResource(R.drawable.short_button);
-        progressReportButton.setTextSize(15);
-        progressReportButton.setText(R.string.progress_report_button);
+            progressReportButton.setBackgroundResource(R.drawable.short_button);
+            progressReportButton.setTextSize(15);
+            progressReportButton.setText(R.string.progress_report_button);
 
-        // This is where we would probably hook up the button's intent
+            // This is where we would probably hook up the button's intent
 
-        // Create the linear layout and the padding
-        LinearLayout buttons = new LinearLayout(this);
-        buttons.setPaddingRelative(40, 60, 40, 0);
-        buttons.setOrientation(LinearLayout.HORIZONTAL);
+            // Create the linear layout and the padding
+            LinearLayout buttons = new LinearLayout(this);
+            buttons.setPaddingRelative(40, 60, 40, 0);
+            buttons.setOrientation(LinearLayout.HORIZONTAL);
 
-        // Bundle the buttons
-        buttons.addView(studentButton);
-        buttons.addView(progressReportButton);
+            // Bundle the buttons
+            buttons.addView(studentButton);
+            buttons.addView(progressReportButton);
 
-        // Show in the view
-        studentAndProgressButtons.addView(buttons);
+            // Show in the view
+            studentAndProgressButtons.addView(buttons);
 
-        //}
+        }
 
     }
 }
