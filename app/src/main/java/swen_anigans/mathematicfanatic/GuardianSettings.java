d24f8966a7a889b72b4bc0df64aa6d1897c9ca42
Sean Klei <stk1151@rit.edu>
2016-11-08 12:45:08 -0500

    Updated the guardian settings to use the custom toolbar and the theme

--- app/src/main/java/swen_anigans/mathematicfanatic/GuardianSettings.java
+++ app/src/main/java/swen_anigans/mathematicfanatic/GuardianSettings.java
@@ -2,12 +2,17 @@ package swen_anigans.mathematicfanatic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class GuardianSettings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.GuardianTheme);
         setContentView(R.layout.activity_guardian_settings);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar.setBackgroundResource(R.color.guardianPrimary);
+        setSupportActionBar(myToolbar);
     }
 }
