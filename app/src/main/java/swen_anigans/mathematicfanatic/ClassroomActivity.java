0ef1cb3c0f2a92aebfe8a27cfab66cb5784669c1
Matthew Gallagher <Matthew Gallagher>
2016-11-10 14:06:54 -0500

    add classroom page, and started layout

---
+++ app/src/main/java/swen_anigans/mathematicfanatic/ClassroomActivity.java
@@ -0,0 +1,23 @@
+package swen_anigans.mathematicfanatic;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+public class ClassroomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classroom);
+
+        /*
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+        */
+    }
+}
