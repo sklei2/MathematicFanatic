ab1913f25c9428e83d26ff8a188d1b8584949b47
Emily Filmer <Emily Filmer>
2016-11-10 15:13:43 -0500

    fix MainActivity.java extra stuff not needed

--- app/src/main/java/swen_anigans/mathematicfanatic/MainActivity.java
+++ app/src/main/java/swen_anigans/mathematicfanatic/MainActivity.java
@@ -1,7 +1,5 @@
 package swen_anigans.mathematicfanatic;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -10,13 +8,6 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import android.view.View;
-import android.widget.EditText;
-
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,5 +35,4 @@ public class MainActivity extends AppCompatActivity {
         // add the adapter to the list
         studentButtons.setAdapter(adapter);
     }
-
 }
