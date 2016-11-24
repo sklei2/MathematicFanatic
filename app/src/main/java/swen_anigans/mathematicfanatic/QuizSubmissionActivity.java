1c84ba68e1150cd6eeeceeb6b1f1a0f19df0b52b
Sean Klei <stk1151@rit.edu>
2016-11-24 13:09:29 -0500

    Added the same dialog to the submission back button

--- app/src/main/java/swen_anigans/mathematicfanatic/QuizSubmissionActivity.java
+++ app/src/main/java/swen_anigans/mathematicfanatic/QuizSubmissionActivity.java
@@ -1,9 +1,12 @@
 package swen_anigans.mathematicfanatic;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 import java.util.ArrayList;
@@ -46,5 +49,33 @@ public class QuizSubmissionActivity extends AppCompatActivity {
         quizIntent.putExtra("atBeginning", false);
 
         startActivity(quizIntent);
+    }
+
+    // Override the back button to first ask for confirmation
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item)
+    {
+        AlertDialog.Builder alert;
+        alert = new AlertDialog.Builder(this);
+
+        alert.setMessage("Leave the quiz?");
+        alert.setPositiveButton("Leave", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent intent = new Intent(QuizSubmissionActivity.this, StudentLocker.class);
+                startActivity(intent);
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // canceled going back.
+            }
+        });
+
+        AlertDialog dialog = alert.create();
+        dialog.show();
+        return true;
     }
 }
